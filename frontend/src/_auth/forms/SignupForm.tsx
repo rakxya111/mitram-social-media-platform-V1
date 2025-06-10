435681c616718317b3c7c5d800f359e90a269167
rakxya111 <rakshyabhuju@gmail.com>
2025-06-10 18:18:14 +0545

    fixed

--- frontend/src/_auth/forms/SignupForm.tsx
+++ frontend/src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
+import {Form,FormControl,FormField,FormItem,FormLabel,FormMessage,} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
