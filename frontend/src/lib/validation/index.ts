056f4cc29ea2b39810798c5fa24565e38d7279db
rakxya111 <rakshyabhuju@gmail.com>
2025-06-12 22:03:29 +0545

    implemented the create post UI

--- frontend/src/lib/validation/index.ts
+++ frontend/src/lib/validation/index.ts
@@ -1,15 +1,20 @@
-import { z } from "zod"
+import { z } from "zod";
 
 export const SignupValidation = z.object({
   name: z.string().min(2, {message : 'The name is too short'}),
   username: z.string().min(2, {message : "The username is too Short"}),
   email: z.string().email(),
   password: z.string().min(8,{ message : 'The password must of atleast 8 characters .'}),
-})
+});
 
 export const SigninValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8,{ message : 'The password must of atleast 8 characters .'}),
-})
+});
 
-
+export const PostValidation = z.object({
+  caption : z.string().min(5).max(2200),
+  file: z.custom<File[]>(),
+  location: z.string().min(2).max(100),
+  tags: z.string(),
+});
